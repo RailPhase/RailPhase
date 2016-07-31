86e5824d4c7b30d52478534824697a3caf38f9c1
Lukas Boersma <lukas.boersma@rwth-aachen.de>
2016-07-31 22:30:43 +0200

    Version bump to 1.1.0

--- RailPhase/Properties/AssemblyInfo.cs
+++ RailPhase/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RailPhase")]
-[assembly: AssemblyDescription("A web framework for .NET")]
+[assembly: AssemblyDescription("A web framework for .NET. https://railphase.github.io")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lukas Boersma")]
 [assembly: AssemblyProduct("RailPhase")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
